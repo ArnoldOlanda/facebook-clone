f13b7d2eb7a351bc933a6c30e3870b7cbc0d7132
gabriel <gabriel_tuco@hotmail.com>
2022-07-09 13:10:06 -0500

    creacion del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,33 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { NavbarFb } from './components/Navbar'
+import { Home } from './components/Home'
+import { People } from './components/People'
+import { Watch } from './components/Watch'
+import { Store } from './components/Store'
+import { Games } from './components/Games'
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <BrowserRouter className="App">
+      <NavbarFb/>
+      <Routes>
+        
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/people" element={<People/>}></Route>
+        <Route path="/watch" element={<Watch/>}></Route>
+        <Route path="/store" element={<Store/>}></Route>
+        <Route path="/games" element={<Games/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
